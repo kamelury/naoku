f593a55b6c2d5d6fa9993cda657f5f374034965c
Helena <helenatxu@Oberon.local>
2010-05-31 13:34:28 +0200

    design changes and .gitignore

---
+++ db/migrate/20100519190003_add_data.rb
@@ -0,0 +1,53 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 3rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails3 for more book information.
+#---
+
+class AddData < ActiveRecord::Migration
+  def self.up
+    Product.delete_all
+
+    Product.create(:title => 'Red Bag',
+    :description => 
+    %{<p>Long handle bag made of synthetic leather. Dimensions: 35 x 33 x 9 cm.
+	</p>},
+    :image_url => '/images/red.jpg',
+    :price => 27.75)
+	
+
+    Product.create(:title => 'Blue Bag',
+    :description => 
+    %{<p>Bolso de mano fabricado en piel bovina. Incluye bandolera interior para colgar. Medidas: 22 x 28 x 11 cms.
+      </p>},
+    :image_url =>   '/images/blue.jpg',    
+    :price => 29.95)
+
+
+    Product.create(:title => 'Black Bag',
+    :description => 
+    %{<p>Long handle bag made of skin. Including that based portfolio - Removable inner wallet. Dimensions: 25 x 36 12 cms.
+	</p>},
+    :image_url => '/images/black.jpg',
+    :price => 27.75)
+	
+	
+    Product.create(:title => 'Yellow Bag',
+      :description =>
+      %{<p>Long handle bag made of synthetic leather. Dimensions: 44 x 29 x 12 cms.
+	  </p>},
+    :image_url => '/images/yellow.jpg',
+    :price => 28.50)
+
+
+
+  end
+
+  def self.down
+    Product.delete_all
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100511103105) do
+ActiveRecord::Schema.define(:version => 20100519190003) do
 
   create_table "line_items", :force => true do |t|
     t.integer  "product_id",                                :null => false
