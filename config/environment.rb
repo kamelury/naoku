2cc7bda2d0f2d398b7b9e3e6368f5e672d57e6b4
kamelury <kamelury@msn.com>
2011-06-19 22:32:42 -0400

    zmiana miejsa require smtp

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'smtp_tls'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -40,7 +41,7 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
-require 'smtp_tls'
+
 
 config.action_mailer.delivery_method = :smtp 
 config.action_mailer.smtp_settings = {
